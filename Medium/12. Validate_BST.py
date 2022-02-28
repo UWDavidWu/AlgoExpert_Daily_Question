982aabd98276a7a58fd410dcb389037badede158
UWDavidWu <wu.dinghang@outlook.com>
2022-02-28 09:56:54 +0800

    change

--- Medium/12. Validate_BST.py
+++ Medium/12. Validate_BST.py
@@ -1,35 +1,23 @@
 '''
-Date: 2022/02/24
+Date: 2022/02/25
 Author: David Wu
-Link: https://www.algoexpert.io/questions/BST%20Traversal
+Link: https://www.algoexpert.io/questions/Validate%20BST
 
 Question Summary:
-Inorder, Preorder, Postorder
+validate BST
 
 Optimal Complexity: O(n) || O(n)
 '''
 
-def inOrderTraverse(tree, array):
-    if tree is not None:
-        inOrderTraverse(tree.left, array)
-        array.append(tree.value)
-        inOrderTraverse(tree.right, array)
-    return array
+def validateBst(tree):
+    return helper(tree,float("-inf"),float("inf"))
 
-def preOrderTraverse(tree, array):
-    if tree is not None:
-        array.append(tree.value)
-        preOrderTraverse(tree.left, array)
-        
-        preOrderTraverse(tree.right, array)
-    return array
-
-def postOrderTraverse(tree, array):
-    if tree is not None:
-        postOrderTraverse(tree.left, array)
-        postOrderTraverse(tree.right, array)
-        array.append(tree.value)
-    return array
+def helper(tree, minVal, maxVal):
+	if tree is None:
+		return True
+	if tree.value < minVal or tree.value >= maxVal:
+		return False
+	return helper(tree.left, minVal, tree.value) and helper(tree.right, tree.value, maxVal)
 
 
     
